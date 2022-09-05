309458c78a446d9740ce7b8387ea6b77db0303b8
Pablo Emanuel <pabloeapereira@gmail.com>
2022-09-05 09:07:26 -0300

    Api Clientes - Definindo entidades

---
+++ src/services/NSE.Cliente.API/Configuration/ApiConfig.cs
@@ -0,0 +1,41 @@
+﻿namespace NSE.Cliente.API.Configuration
+{
+    public static class ApiConfig
+    {
+        public static IServiceCollection AddApiConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            //services.AddDbContext<CatalogoContext>(options =>
+            //   options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddControllers();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("Total", builder =>
+                    builder
+                        .AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+            });
+
+            return services;
+        }
+
+        public static WebApplication UseApiConfiguration(this WebApplication app)
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            //app.UseAuthConfiguration();
+            app.UseCors("Total");
+
+            app.MapControllers();
+
+            return app;
+        }
+    }
+}
