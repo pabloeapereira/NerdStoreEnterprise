2d0f1732fad81a5f96cea348d66425477416b382
Pablo Emanuel <pabloeapereira@gmail.com>
2022-09-06 14:40:52 -0300

    API Clientes - Validação de comandos

--- src/services/NSE.Cliente.API/Application/Commands/RegistrarClienteCommand.cs
+++ src/services/NSE.Cliente.API/Application/Commands/RegistrarClienteCommand.cs
@@ -1,4 +1,6 @@
-﻿using NSE.Core.Messages;
+﻿using FluentValidation;
+using FluentValidation.Results;
+using NSE.Core.Messages;
 
 namespace NSE.Clientes.API.Application.Commands
 {
@@ -16,7 +18,30 @@ namespace NSE.Clientes.API.Application.Commands
             Email = email;
             Cpf = cpf;
         }
-    }
 
+        public override bool IsValid()
+        {
+            ValidationResult = new RegistrarClienteValidation().Validate(this);
+            return ValidationResult.IsValid;
+        }
+
+        internal class RegistrarClienteValidation : AbstractValidator<RegistrarClienteCommand>
+        {
+            public RegistrarClienteValidation()
+            {
+                RuleFor(c => c.Id).NotEqual(Guid.Empty).WithMessage("O campo {PropertyName} do cliente inválido.");
+
+                RuleFor(c => c.Nome).NotEmpty().WithMessage("O campo {PropertyName} do cliente não foi informado.")
+                    .MaximumLength(200).WithMessage("O campo {PropertyName} do cliente deve ter no máximo {MaxLength}");
     
+            RuleFor(c => c.Cpf).Must(CpfValid).WithMessage("O campo {PropertyName} informado não é válido.");
+
+                RuleFor(c => c.Email).Must(EmailValid).WithMessage("O campo {PropertyName} informado não é válido.");
+            }
+
+            protected bool CpfValid(string cpf) => Core.DomainObjects.Cpf.Validar(cpf);
+
+            protected bool EmailValid(string email) => Core.DomainObjects.Email.Validar(email);
+        }
+    }
 }
