d9a44cebebfc0dcb3b7f968adee8316cf9842258
Pablo Emanuel <pabloeapereira@gmail.com>
2023-01-08 12:31:47 -0300

    Baixando pedidos do estoque

---
+++ src/services/NSE.Catalogo.API/Configuration/MessageBusConfig.cs
@@ -0,0 +1,16 @@
+﻿using NSE.Catalogo.API.Services;
+using NSE.Core.Extensions;
+using NSE.MessageBus;
+
+namespace NSE.Catalogo.API.Configuration
+{
+    public static class MessageBusConfig
+    {
+        public static void AddMessageBusConfiguration(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddMessageBus(configuration.GetMessageQueueConnection("MessageBus"))
+                .AddHostedService<CatalogoIntegrationHandler>();
+        }
+    }
+}
